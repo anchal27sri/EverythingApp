c7ba7d5c907882cc98a1c9f0a694b8d614cdb5fe
anchal27sri <srivastavaanchal000@gmail.com>
2023-12-24 22:54:14 +0530

    deleted useless stuff

--- app/src/main/java/com/example/everythingapp/converter/ui/ConverterText.kt
+++
@@ -1,2 +0,0 @@
-package com.example.everythingapp.converter.ui
-
--- app/src/main/java/com/example/everythingapp/converter/ui/texts/EditableText.kt
+++ app/src/main/java/com/example/everythingapp/converter/ui/texts/EditableText.kt
@@ -3,7 +3,6 @@ package com.example.everythingapp.converter.ui.texts
 import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
